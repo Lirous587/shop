cf49cd7ca83d62094d0e47e953e0d11e75cd0dcf
lzh <2115883273@qq.com>
2024-01-19 23:41:34 +0800

    vue基本框架

---
+++ src/pages/404.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <el-result
+        icon="warning"
+        title="404提升"
+        sub-title="你找的页面走丢了"
+      >
+        <template #extra>
+          <el-button type="primary" @click= "redirectToHome">回到首页</el-button>
+        </template>
+      </el-result>
+    </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    redirectToHome() {
+      this.$router.push("/");
+      }
+    }
+  }
+</script>
