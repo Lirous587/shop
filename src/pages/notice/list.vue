d787d9b8b79205d48b85465c65addbbfad312b8d
lzh <2115883273@qq.com>
2024-04-21 16:10:08 +0800

    公告列表分页互交

--- src/pages/notice/list.vue
+++ src/pages/notice/list.vue
@@ -1,21 +1,20 @@
 <template>
     <el-card shadow="always" :body-style="{ padding: '20px' }">
-        <!-- 新增 | 刷新 -->
         <div class="flex items-center  justify-between mb-4">
             <el-button type="primary" size="default" @click="">新增</el-button>
 
             <el-tooltip class="box-item" effect="dark" content="Top Right prompts info" placement="top-end">
-                <el-button text>
+                <el-button text @click="getData">
                     <el-icon :size="20">
                         <Refresh />
                     </el-icon>
                 </el-button>
             </el-tooltip>
         </div>
-        <el-table :data="tableData" stripe style="width: 100%">
+
+        <el-table :data="tableData" stripe style="width: 100%" v-loading="loading">
             <el-table-column prop="title" label="公告标题" />
             <el-table-column prop="create_time" label="发布时间" width="380" />
-
             <el-table-column label="操作" width="150" align="center">
                 <template #default="scope">
                     <el-button type="primary" size="small" text @click="">修改</el-button>
@@ -27,7 +26,6 @@
                             </template>
                         </el-popconfirm>
                     </span>
-
                 </template>
             </el-table-column>
 
@@ -35,24 +33,44 @@
     </el-card>
 
 
-
+    <div class="flex justify-center items-center mt-4">
+        <el-pagination background layout="prev, pager,next" :current-page="currentPage" @current-change="getData"
+            :total="total" />
+    </div>
 
 
 </template>
 
 <script setup>
 import { ref } from "vue";
+import {
+    getNoticeList
+} from "~/api/notice";
+
+
 const tableData = ref([])
 
-function getData() {
-    tableData.value = [{
-        "id": 13,
-        "title": "nip",
-        "content": "nip\n",
-        "order": 0,
-        "create_time": "2022-06-06 14:40:11",
-        "update_time": "2022-06-06 14:40:11"
-    }]
+const loading = ref(false)
+
+//分页
+const currentPage = ref(1)
+const total = ref(0)
+const list = ref([])
+
+
+function getData(p = null) {
+    if (typeof p == Number) {
+        currentPage.value = p
+    }
+    loading.value = true
+    getNoticeList(currentPage.value)
+        .then(res => {
+            tableData.value = res.list
+            total.value = res.totalCount
+        })
+        .finally(() => {
+            loading.value = false
+        })
 }
 
 getData()
